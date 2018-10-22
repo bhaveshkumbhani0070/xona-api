5cad27f86e15841822c83791780f8754fcc3d790
kumbhani bhavesh <kumbhani.bhavesh.1@gmail.com>
2018-10-22 14:51:27 +0530

    Update for new field in sign up

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -10,96 +10,126 @@ const fun=require('./function');
 
 //Simple version, without validation or sanitation
 exports.signup = function (req, res) {
+    console.log("*** Requested for Creating New User... ***");
+    receivedValues = req.body
+    if (JSON.stringify(receivedValues) === '{}')
+    {
+        res.status(400).json({status:false,message:'Please fill the fields'});
+        return;
+    }
+    else
+    {
 
-    User.find({mobile:req.body.mobile},function(err,oldUser){
-        if(!err){
-            if(oldUser.length>0){
-                console.log('User alredy register with this mobile number');
-                res.status(400).json({status:false,message:'User alredy register with this mobile number. Please login.'});
+         usercolumns = [
+             "firstname", "lastname", "mobile",
+             "email", "alternate_email","dob",
+             "merriag_date", "area", "pincode",
+             "city", "country", "gender",
+             "reffaral_id","code", "childCode",
+             "gst_no", "verified","create_at"
+            ];
+
+        var dbValues = [];
+        var checkProfessional = false;
+        for (var iter = 0; iter < usercolumns.length; iter++) {
+            columnName = usercolumns[iter];
+
+            if ((receivedValues[columnName] == undefined || receivedValues[columnName] == "") && (columnName == 'firstname' || columnName == 'lastname' || columnName=='mobile' ))
+            {
+                console.log("*** Redirecting: ", columnName, " field is required");
+                res.json({"code": 400, "status": "Error", "message": columnName + " field is undefined"});
+                logger.error('*** Redirecting: ', columnName, ' field is required');
                 return;
             }
-            else{
-                 // Will send otp on mobile
-                sendOTP(req.body.mobile,"PRIIND");
-                // Genrate uniq code for user which will use as a refrence code
-                genrateCode(function(cData){
-                    //check code is found in database or not
-                    if(req.body.parentCode){
-                        User.find({code:req.body.parentCode},function(err,parentData){
-                            if(!err){
-                                if(parentData.length>0){
-                                    console.log('data found');
-                                    User.findByIdAndUpdate(parentData[0]._id, {$push: {childCode:cData}}, function (err, product) {
-                                        if (err){
-                                            console.log('Error for update ');
-                                            return;
-                                            // res.json({code:200,status:'error',message:'Error for update'});
-                                            // return;
-                                        }
-                                        else{
-                                            // Create new user with parent code and child code blank for feature update
-                                            console.log('Error for push child code into child array');
-                                            return;
-                                        }
-                                    });
-                                }
-                                else{
-                                    console.log('data not found');
-                                    return;
-                                }
-                            }
-                            else{
-                                res.status(500).json({ status: false,message:'Error for get a data for parent' })
-                                return;
-                            }
-                        })
-                    }
-
-                    let user = new User(
-                        {
-                            firstname: req.body.firstname,
-                            lastname:req.body.lastname,
-                            mobile:req.body.mobile,
-                            email:req.body.email,
-                            alternate_email:req.body.alternate_email,
-                            dob:req.body.dob,
-                            merriag_date:req.body.merriag_date,
-                            area:req.body.area,
-                            pincode:req.body.pincode,
-                            city:req.body.city,
-                            country:req.body.country,
-                            gender:req.body.gender,
-                            code:cData,
-                            reffaral_id:req.body.reffaral_id,
-                            childCode:[],
-                            gst_no:req.body.gst_no,
-                            verified:false,
-                            create_at:new Date()
-                        }
-                    );
-
-                    user.save(function (err,data) {
-                        if (err) {
-                            console.log('Error for save data',err);
-                            res.status(500).json({ status: false,message:'Error for save user details' });
-                            return;
-                        }
-                        else{
-                            res.status(200).json({ status: true,message:'User created successfull !',data:data });
-                            return;
-                        }
-                    })
-
-                });
+            if (receivedValues[columnName] == undefined || receivedValues[columnName] == "")
+            {
+                dbValues[iter] = '';
+                receivedValues[columnName]='';
+            }
+            else
+            {
+                dbValues[iter] = receivedValues[columnName];
             }
         }
-        else{
-            console.log('Error for get user details');
-            res.status(500).json({ status: false,message:'Error for find user detail' });
-            return;
-        }
-    });
+        receivedValues.create_at=new Date();
+        receivedValues.verified=false;
+        console.log('dbValues',receivedValues);
+        genrateCode(function(cData){
+            receivedValues.code=cData;
 
+            User.find({mobile:req.body.mobile},function(err,oldUser){
+                if(!err){
+                    if(oldUser.length>0){
+                        console.log('User alredy register with this mobile number');
+                        res.status(400).json({status:false,message:'User alredy register with this mobile number. Please login.'});
+                        return;
+                    }
+                    else{
+                        // Will send otp on mobile
+                        sendOTP(req.body.mobile,"PRIIND");
+                        // Genrate uniq code for user which will use as a refrence code
+
+                            //check code is found in database or not
+                            if(req.body.parentCode){
+                                User.find({code:req.body.parentCode},function(err,parentData){
+                                    if(!err){
+                                        if(parentData.length>0){
+                                            console.log('data found');
+                                            User.findByIdAndUpdate(parentData[0]._id, {$push: {childCode:cData}}, function (err, product) {
+                                                if (err){
+                                                    console.log('Error for update ');
+                                                    return;
+                                                    // res.json({code:200,status:'error',message:'Error for update'});
+                                                    // return;
+                                                }
+                                                else{
+                                                    // Create new user with parent code and child code blank for feature update
+                                                    console.log('Error for push child code into child array');
+                                                    return;
+                                                }
+                                            });
+                                        }
+                                        else{
+                                            console.log('data not found');
+                                            return;
+                                        }
+                                    }
+                                    else{
+                                        res.status(500).json({ status: false,message:'Error for get a data for parent' })
+                                        return;
+                                    }
+                                })
+                            }
+
+                            let user = new User(receivedValues);
+
+                            user.save(function (err,data) {
+                                if (err) {
+                                    console.log('Error for save data',err);
+                                    res.status(500).json({ status: false,message:'Error for save user details' });
+                                    return;
+                                }
+                                else{
+                                    res.status(200).json({ status: true,message:'User created successfull !',data:data });
+                                    return;
+                                }
+                            })
+
+                    }
+                }
+                else{
+                    console.log('Error for get user details');
+                    res.status(500).json({ status: false,message:'Error for find user detail' });
+                    return;
+                }
+            });
+        });
+
+
+
+
+
+    }
 
 };
 
