f298b6ec689c962be8b427388aace7277e89d522
kumbhani bhavesh <kumbhani.bhavesh.1@gmail.com>
2018-10-22 09:13:28 +0530

    Update for sign up api

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -19,7 +19,68 @@ exports.signup = function (req, res) {
                 return;
             }
             else{
+                 // Will send otp on mobile
+                sendOTP(req.body.mobile,"PRIIND");
 
+                // Genrate uniq code for user which will use as a refrence code
+                genrateCode(function(cData){
+
+                    //check code is found in database or not
+                    if(req.body.parentCode){
+                        User.find({code:req.body.parentCode},function(err,parentData){
+                            if(!err){
+                                if(parentData.length>0){
+                                    console.log('data found');
+                                    User.findByIdAndUpdate(parentData[0]._id, {$push: {childCode:cData}}, function (err, product) {
+                                        if (err){
+                                            console.log('Error for update ');
+                                            return;
+                                            // res.json({code:200,status:'error',message:'Error for update'});
+                                            // return;
+                                        }
+                                        else{
+                                            // Create new user with parent code and child code blank for feature update
+                                            console.log('Error for push child code into child array');
+                                            return;
+                                        }
+                                    });
+                                }
+                                else{
+                                    console.log('data not found');
+                                    return;
+                                }
+                            }
+                            else{
+                                res.status(500).json({ status: false,message:'Error for get a data for parent' })
+                                return;
+                            }
+                        })
+                    }
+
+                    let user = new User(
+                        {
+                            name: req.body.name,
+                            mobile:req.body.mobile,
+                            code:cData,
+                            parentCode:req.body.parentCode,
+                            childCode:[],
+                            verified:false,
+                            create_at:new Date()
+                        }
+                    );
+
+                    user.save(function (err,data) {
+                        if (err) {
+                            console.log('Error',err);
+                            return;
+                        }
+                        else{
+                            res.status(200).json({ status: true,message:'User created successfull !',data:data });
+                            return;
+                        }
+                    })
+
+                });
             }
         }
         else{
@@ -27,68 +88,7 @@ exports.signup = function (req, res) {
         }
     });
 
-    // Will send otp on mobile
-    sendOTP(req.body.mobile,"PRIIND");
 
-    // Genrate uniq code for user which will use as a refrence code
-    genrateCode(function(cData){
-
-        //check code is found in database or not
-        if(req.body.parentCode){
-            User.find({code:req.body.parentCode},function(err,parentData){
-                if(!err){
-                    if(parentData.length>0){
-                        console.log('data found');
-                        User.findByIdAndUpdate(parentData[0]._id, {$push: {childCode:cData}}, function (err, product) {
-                            if (err){
-                                console.log('Error for update ');
-                                return;
-                                // res.json({code:200,status:'error',message:'Error for update'});
-                                // return;
-                            }
-                            else{
-                                // Create new user with parent code and child code blank for feature update
-                                console.log('Error for push child code into child array');
-                                return;
-                            }
-                        });
-                    }
-                    else{
-                        console.log('data not found');
-                        return;
-                    }
-                }
-                else{
-                    res.status(500).json({ status: false,message:'Error for get a data for parent' })
-                    return;
-                }
-            })
-        }
-
-        let user = new User(
-            {
-                name: req.body.name,
-                mobile:req.body.mobile,
-                code:cData,
-                parentCode:req.body.parentCode,
-                childCode:[],
-                verified:false,
-                create_at:new Date()
-            }
-        );
-
-        user.save(function (err,data) {
-            if (err) {
-                console.log('Error',err);
-                return;
-            }
-            else{
-                res.status(200).json({ status: true,message:'User created successfull !',data:data });
-                return;
-            }
-        })
-
-    });
 };
 
 
